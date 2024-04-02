393d0da7d5543240ab97be2cd43da2ea536a5c34
eminencegrs <a.dariienko@outlook.com>
2024-04-02 10:28:37 +0200

    Add a solution to the 'Length of Last Word' problem.

---
+++ LeetCode/src/LeetCode.Challenges/LengthOfLastWord/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Challenges.LengthOfLastWord;
+
+public class Solution
+{
+    // The complexity is O(n).
+    public static int GetLengthOfLastWordV1(string s)
+    {
+        return s.Split(' ').Last().Length;
+    }
+
+    // The complexity is O(1).
+    public static int GetLengthOfLastWordV2(string phrase)
+    {
+        var length = 0;
+        var index = phrase.Length - 1;
+
+        // Skip trailing whitespace. 
+        while (index >= 0 && phrase[index] == ' ')
+        {
+            index--;
+        }
+
+        // Count characters of the last word until the first space or the beginning of the string.
+        while (index >= 0 && phrase[index] != ' ')
+        {
+            length++;
+            index--;
+        }
+
+        return length;
+    }
+}
