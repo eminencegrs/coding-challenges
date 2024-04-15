291a6d8a95c22ccb5cff4716142c688605bd752d
eminencegrs <a.dariienko@outlook.com>
2024-04-15 20:59:09 +0200

    Add a solution to the 'ClimbingStairs_MyVersion' problem.

---
+++ LeetCode/tests/LeetCode.Challenges.UnitTests/ClimbingStairs_MyVersion/ClimbingStairsTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Challenges.ClimbingStairs_MyVersion;
+using Shouldly;
+using Xunit;
+
+namespace LeetCode.Challenges.UnitTests.ClimbingStairs_MyVersion;
+
+public class ClimbingStairsTests
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void GivenNumbers_WhenAddTwoNumbers_ThenResultAsExpected(int n, int expectedResult)
+    {
+        var actualResult = new Solution().ClimbStairs(n);
+        actualResult.ShouldBeEquivalentTo(expectedResult);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return [ 2, 2 ];
+        yield return [ 3, 4 ];
+        yield return [ 5, 13 ];
+        yield return [ 7, 44 ];
+    }
+}
