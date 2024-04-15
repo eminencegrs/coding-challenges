8efd1ffcbfa2f2a2101e125808d60bc63f525636
eminencegrs <a.dariienko@outlook.com>
2024-04-15 18:38:17 +0200

    Add a solution to the 'Climbing Stairs' problem.

---
+++ LeetCode/src/LeetCode.Challenges/ClimbingStairs/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Challenges.ClimbingStairs;
+
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        var cache = new Dictionary<int, int>();
+        return this.Climb(n, cache);
+    }
+
+    private int Climb(int n, IDictionary<int, int> cache)
+    {
+        if (n < 0)
+        {
+            return 0;
+        }
+
+        if (n == 0)
+        {
+            return 1;
+        }
+
+        if (cache.TryGetValue(n, out var value))
+        {
+            return value;
+        }
+
+        cache[n] = this.Climb(n - 1, cache) + this.Climb(n - 2, cache);
+        return cache[n];
+    }
+}
---
+++ LeetCode/tests/LeetCode.Challenges.UnitTests/ClimbingStairs/ClimbingStairsTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Challenges.ClimbingStairs;
+using Shouldly;
+using Xunit;
+
+namespace LeetCode.Challenges.UnitTests.ClimbingStairs;
+
+public class ClimbingStairsTests
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void GivenNumbers_WhenAddTwoNumbers_ThenResultAsExpected(int n, int expectedResult)
+    {
+        var actualResult = new Solution().ClimbStairs(n);
+        actualResult.ShouldBeEquivalentTo(expectedResult);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return [ 2, 2 ];
+        yield return [ 3, 3 ];
+        yield return [ 5, 8 ];
+    }
+}
