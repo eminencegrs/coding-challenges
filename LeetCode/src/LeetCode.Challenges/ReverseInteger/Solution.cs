a919e3c8a72ef3c6b301dd739f40507154f676b7
eminencegrs <a.dariienko@outlook.com>
2024-05-24 09:37:40 +0200

    Add a solution to the 'Reverse Integer' problem.

---
+++ LeetCode/src/LeetCode.Challenges/ReverseInteger/Solution.cs
@@ -0,0 +1,29 @@
+namespace LeetCode.Challenges.ReverseInteger;
+
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        int result = 0;
+
+        while (x != 0)
+        {
+            int temp = x % 10;
+            x /= 10;
+
+            if (result > int.MaxValue / 10 || (result == int.MaxValue / 10 && temp > 7))
+            {
+                return 0;
+            }
+
+            if (result < int.MinValue / 10 || (result == int.MinValue / 10 && temp < -8))
+            {
+                return 0;
+            }
+
+            result = result * 10 + temp;
+        }
+
+        return result;
+    }
+}
