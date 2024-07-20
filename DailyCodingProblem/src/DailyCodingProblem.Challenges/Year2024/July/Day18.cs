931d6d5125f03be7c371cd964fd24b2fdf64bef4
eminencegrs <a.dariienko@outlook.com>
2024-07-20 13:29:01 +0200

    Daily Coding Problem. 2024, July. Day 18.

---
+++ DailyCodingProblem/src/DailyCodingProblem.Challenges/Year2024/July/Day18.cs
@@ -0,0 +1,40 @@
+﻿namespace DailyCodingProblem.Challenges.Year2024.July;
+
+// This problem was asked by Google.
+// Given a list of numbers and a number `k`, return whether any two numbers from the list add up to `k`.
+// For example, given `[10, 15, 3, 7]` and `k` of `17`, return true since `10 + 7` is `17`.
+// Bonus: Can you do this in one pass?
+public static class Day18
+{
+    public static bool GetResult(int[] numbers, int target)
+    {
+        if (numbers.Length == 0)
+        {
+            return false;
+        }
+
+        Array.Sort(numbers);
+
+        var leftPointer = 0;
+        var rightPointer = numbers.Length - 1;
+
+        while (leftPointer < rightPointer)
+        {
+            if (numbers[leftPointer] + numbers[rightPointer] == target)
+            {
+                return true;
+            }
+
+            if (numbers[leftPointer] + numbers[rightPointer] < target)
+            {
+                leftPointer++;
+            }
+            else
+            {
+                rightPointer--;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ DailyCodingProblem/tests/DailyCodingProblem.Challenges.UnitTests/Year2024/July/Day18Tests.cs
@@ -0,0 +1,25 @@
+using DailyCodingProblem.Challenges.Year2024.July;
+using Shouldly;
+using Xunit;
+
+namespace DailyCodingProblem.Challenges.UnitTests.Year2024.July;
+
+public class Day18Tests
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void GivenArray_WhenGetResult_ThenResultAsExpected(int[] numbers, int target, bool expectedResult)
+    {
+        bool actualResult = Day18.GetResult(numbers, target);
+        actualResult.ShouldBeEquivalentTo(expectedResult);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return [ new [] { 10, 15, 3, 7 }, 17, true ];
+        yield return [ new [] { 10, 7, 6, 3, 1, 0 }, 15, false ];
+        yield return [ new [] { 21, -13, -5, 14, -10, 8, 0, -1, 5, 100 }, 100, true ];
+        yield return [ new [] { 21, -13, -5, 14, -10, 8, 0, -1, 5, 100 }, 0, true ];
+        yield return [ new [] { 21, -13, -5, 14, -10, 8, 0, -1, 5, 100 }, 200, false ];
+    }
+}
