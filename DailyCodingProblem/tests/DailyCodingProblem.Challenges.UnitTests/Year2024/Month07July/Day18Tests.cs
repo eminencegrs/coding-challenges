cdb650ac763f0cc82331e8f1e2960606ae92fed9
eminencegrs <a.dariienko@outlook.com>
2024-08-14 15:31:58 +0200

    Add unit tests.

--- DailyCodingProblem/tests/DailyCodingProblem.Challenges.UnitTests/Year2024/Month07July/Day18Tests.cs
+++ DailyCodingProblem/tests/DailyCodingProblem.Challenges.UnitTests/Year2024/Month07July/Day18Tests.cs
@@ -16,6 +16,7 @@ public class Day18Tests
 
     public static IEnumerable<object[]> TestData()
     {
+        yield return [ Array.Empty<int>(), int.MaxValue, false ];
         yield return [ new [] { 10, 15, 3, 7 }, 17, true ];
         yield return [ new [] { 10, 7, 6, 3, 1, 0 }, 15, false ];
         yield return [ new [] { 21, -13, -5, 14, -10, 8, 0, -1, 5, 100 }, 100, true ];
