1301bd05c59eb237adf2d04ec1a8e1c4938ef092
Andy D. <a.dariienko@outlook.com>
2024-09-18 16:08:30 +0200

    Add explanation to NewtonsMethod.cs

--- LeetCode/src/LeetCode.Challenges/SquareRoot/NewtonsMethod.cs
+++ LeetCode/src/LeetCode.Challenges/SquareRoot/NewtonsMethod.cs
@@ -1,5 +1,7 @@
 namespace LeetCode.Challenges.SquareRoot;
 
+// Time Complexity: O(log(n))
+//
 // https://en.wikipedia.org/wiki/Newton%27s_method
 // Newton's method (also known as the Newton-Raphson method) is an iterative numerical technique
 // for finding approximate solutions to equations.
@@ -10,6 +12,11 @@ namespace LeetCode.Challenges.SquareRoot;
 // x_(n+1) = x_n - f(x_n) / f'(x_n).
 // For the problem of finding the square root of a number x, we can set it up as finding
 // the root of the function `f(y) = y^2 - x`. Here we want to find `y` such that `y^2 = x`.
+//
+// Convergence: Quadratic, meaning the number of correct digits roughly doubles with each iteration.
+// Newton’s method is generally more efficient than binary search for finding square roots 
+// because it converges faster (quadratically vs. linearly). 
+// This means Newton’s method usually requires fewer iterations to reach the desired precision.
 public class NewtonsMethod
 {
     public static int GetResult(int x)
