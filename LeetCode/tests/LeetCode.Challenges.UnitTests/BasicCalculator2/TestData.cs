dea3e3922442d7ecb4bc90224c2cd85cf5894143
eminencegrs <a.dariienko@outlook.com>
2024-10-04 22:37:53 +0200

    Add more test cases.

--- LeetCode/tests/LeetCode.Challenges.UnitTests/BasicCalculator2/TestData.cs
+++ LeetCode/tests/LeetCode.Challenges.UnitTests/BasicCalculator2/TestData.cs
@@ -11,6 +11,9 @@ public class TestData : IEnumerable<object[]>
         yield return [ " 3+5 / 2 ", 5 ];
         yield return [ "42", 42 ];
         yield return [ " 3 0* 2 + 4 /3-5 * 1 0", 11 ];
+        yield return [ "100 / 10 / 5 / 2 / 1", 1 ];
+        yield return [ "100 / 3 * 10 + 3", 333 ];
+        yield return [ "1*2-3+4*5", 19 ];
     }
 
     IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
