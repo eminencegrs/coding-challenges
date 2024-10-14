f7a54354ffca42a61deeb59e2026c91ce60bdf1c
eminencegrs <a.dariienko@outlook.com>
2024-10-14 13:07:11 +0200

    HackerRank. Add unit tests.

--- HackerRank/tests/HackerRank.UnitTests/BasicProblemSolvingCert/SubArraySumProblemTests.cs
+++ HackerRank/tests/HackerRank.UnitTests/BasicProblemSolvingCert/SubArraySumProblemTests.cs
@@ -23,5 +23,26 @@ public class SubArraySumProblemTests
             new List<List<int>> { new() { 1, 2, 5 } },
             new List<long> { 15 }
         ];
+
+        yield return
+        [
+            new List<int> { 10, 0, 20 },
+            new List<List<int>> { new() { 1, 2, 10 }, new() { 2, 3, 20 } },
+            new List<long> { 20, 40 }
+        ];
+
+        yield return
+        [
+            new List<int> { 1, 0, 3, 4, 5, 0, 0, 8, 9 },
+            new List<List<int>>
+            {
+                new() { 1, 3, 2 },
+                new() { 4, 6, 4 },
+                new() { 7, 9, 8 },
+                new() { 3, 7, 16 },
+                new() { 1, 9, 32 },
+            },
+            new List<long> { 6, 13, 25, 44, 126 }
+        ];
     }
 }
