9654bb81217d10914958b88a746cd0225d2da692
eminencegrs <a.dariienko@outlook.com>
2024-11-09 13:50:16 +0100

    Solve LeetCode #141 'Linked List Cycle' [Easy].

---
+++ LeetCode/src/LeetCode.Challenges/Problems1XX/P141_LinkedListCycle/ListNode.cs
@@ -0,0 +1,7 @@
+namespace LeetCode.Challenges.Problems1XX.P141_LinkedListCycle;
+
+public class ListNode(int x)
+{
+    public int Value { get; set; } = x;
+    public ListNode? Next { get; set; } = null;
+}
