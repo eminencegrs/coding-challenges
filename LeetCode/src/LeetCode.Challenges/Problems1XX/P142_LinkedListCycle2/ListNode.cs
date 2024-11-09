81c52f79d34698c79ac29a73f5225892a1d7c225
eminencegrs <a.dariienko@outlook.com>
2024-11-09 14:58:36 +0100

    Solve LeetCode #142 'Linked List Cycle II' [Medium].

---
+++ LeetCode/src/LeetCode.Challenges/Problems1XX/P142_LinkedListCycle2/ListNode.cs
@@ -0,0 +1,7 @@
+namespace LeetCode.Challenges.Problems1XX.P142_LinkedListCycle2;
+
+public class ListNode(int x)
+{
+    public int Value { get; set; } = x;
+    public ListNode? Next { get; set; } = null;
+}
