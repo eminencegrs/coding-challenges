d55636a8ba4851a14c45f4e71a9061455eaa37dd
eminencegrs <a.dariienko@outlook.com>
2024-11-09 20:35:20 +0100

    [DRAFT]. Solve LeetCode #74 'Search a 2D Matrix' [Medium].

---
+++ LeetCode/src/LeetCode.Challenges/Problems0XX/P074_Search2DMatrix/Solution.cs
@@ -0,0 +1,11 @@
+namespace LeetCode.Challenges.Problems0XX.P074_Search2DMatrix;
+
+public static class Solution
+{
+    public static bool SearchMatrix(int[][] matrix, int target)
+    {
+        // TODO: implement.
+
+        throw new NotImplementedException();
+    }
+}
