c559bb60e7a369c6cb0658cddef5c0b004b68616
eminencegrs <a.dariienko@outlook.com>
2024-11-11 21:12:30 +0100

    [DRAFT]. Solve LeetCode #10 'Regular Expression Matching' [Hard].

---
+++ LeetCode/src/LeetCode.Challenges/Problems0XX/P010_RegularExpressionMatching/Solution.cs
@@ -0,0 +1,11 @@
+using System.Text;
+
+namespace LeetCode.Challenges.Problems0XX.P010_RegularExpressionMatching;
+
+public static class Solution
+{
+    public static bool IsMatch(string str, string pattern)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ LeetCode/tests/LeetCode.Challenges.UnitTests/Problems0XX/P010_RegularExpressionMatching/SolutionTests.cs
@@ -0,0 +1,24 @@
+using FluentAssertions;
+using LeetCode.Challenges.Problems0XX.P010_RegularExpressionMatching;
+using Xunit;
+
+namespace LeetCode.Challenges.UnitTests.Problems0XX.P010_RegularExpressionMatching;
+
+public class SolutionTests
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void GivenStringAndPattern_WhenCallIsMatch_ThenResultAsExpected(
+        string str, string pattern, bool expectedResult)
+    {
+        var action = () => Solution.IsMatch(str, pattern);
+        action.Should().Throw<NotImplementedException>();
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return ["aa", "a", false];
+        yield return ["aa", "a*", true];
+        yield return ["ab", ".*", true];
+    }
+}
