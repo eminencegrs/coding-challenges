eeb801a17d45e66cb1f1d63afe5be4e3e3320193
eminencegrs <a.dariienko@outlook.com>
2024-11-12 11:55:34 +0100

    Solve LeetCode #24 'Swap Nodes in Pairs' [Medium].

---
+++ LeetCode/src/LeetCode.Challenges/Problems0XX/P024_SwapNodesInPairs/Solutions.cs
@@ -0,0 +1,74 @@
+namespace LeetCode.Challenges.Problems0XX.P024_SwapNodesInPairs;
+
+public static class Solution
+{
+    public static ListNode? SwapPairs(ListNode? head)
+    {
+        // Example:
+        //            Input: 1 --> 2 --> 3 --> 4
+        //  Expected Result: 2 --> 1 --> 4 --> 3
+
+        // Let's introduce a dummy node that points to the head:
+        var dummy = new ListNode(0, head);
+
+        // [dummy,next is [1]] --> [1,next is [2]] --> [2,next is [3]] --> [3,next is [4]] --> [4,next is null]
+
+        // Let's create a few helper pointers:
+        var (prev, current) = (dummy, dummy.next);
+
+        // [dummy,next is [1]] --> [1,next is [2]] --> [2,next is [3]] --> [3,next is [4]] --> [4,next is null]
+        //   ^                      ^
+        // prev                  current
+
+        // Because we need to swap every two adjacent nodes (?).
+        while (current != null && current.next != null)
+        {
+            // STEP 1: Save pointers.
+            var nextPair = current.next.next;
+            var second = current.next;
+
+            // [dummy,next is [1]] --> [1,next is [2]] --> [2,next is [3]] --> [3,next is [4]] --> [4,next is null]
+            //   ^                      ^                   ^                   ^
+            // prev                  current                ^                   ^
+            //                                     current.next (second)        ^
+            //                                                    current.next.next (nextPair)
+
+            // So, 'nextPair' points to [3]. It will be processed in the next iteration.
+            // And 'second' points to [2]. It will be swapped with [1] now.
+
+            // STEP 2: Swap nodes.
+            // Step 2.1:
+            // [2,next is [1]] --> [1,next is [2]]
+            second.next = current;
+
+            // Step 2.2:
+            // [2,next is [1]] --> [1,next is [3]] --> [3,next is [4]]
+            current.next = nextPair;
+
+            // Step 2.3:
+            // [dummy,next is [2]] --> [2,next is [1]] --> [1,next is [3]] --> [3,next is [4]]
+            //   ^                      ^
+            // prev               prev.next
+            prev.next = second;
+
+            // Update pointers.
+            
+            // Before:
+            // [dummy,next is [2]] --> [2,next is [1]] --> [1,next is [3]] --> [3,next is [4]] --> [4,next is null]
+            //  ^                                           ^
+            // prev                                         ^
+            //                                           current
+
+            prev = current;
+            current = nextPair;
+
+            // After:
+            // [dummy,next is [2]] --> [2,next is [1]] --> [1,next is [3]] --> [3,next is [4]] --> [4,next is null]
+            //                                              ^                   ^
+            //                                            prev                  ^
+            //                                                               current
+        }
+
+        return dummy.next;
+    }
+}
