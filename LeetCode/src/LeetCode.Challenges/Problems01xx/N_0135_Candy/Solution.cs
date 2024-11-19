8e49a203d77f78aa65f241fd9cc31c8a9589c43f
eminencegrs <a.dariienko@outlook.com>
2024-11-19 16:33:34 +0100

    Refactoring.

--- LeetCode/src/LeetCode.Challenges/Problems01xx/N_0135_Candy/Solution.cs
+++ LeetCode/src/LeetCode.Challenges/Problems01xx/N_0135_Candy/Solution.cs
@@ -1,5 +1,7 @@
 namespace LeetCode.Challenges.Problems01xx.N_0135_Candy;
 
+// Time Complexity: O(n).
+// Space Complexity: O(n).
 public static class Solution
 {
     public static int Candy(int[] ratings)
@@ -14,8 +16,12 @@ public static class Solution
 
     private static int GetCandies(int[] ratings)
     {
+        // Initialize the candies array with 1 candy for each child.
+        // Each child must have at least one candy.
         var candies = Enumerable.Repeat(1, ratings.Length).ToArray();
 
+        // Left-to-right pass:
+        // Ensure that each child has more candies than their left neighbor if their rating is higher.
         for (var i = 1; i < ratings.Length; i++)
         {
             if (ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1])
@@ -24,6 +30,9 @@ public static class Solution
             }
         }
 
+        // Right-to-left pass:
+        // Ensure that each child has more candies than their right neighbor if their rating is higher.
+        // This ensures the final allocation is correct.
         for (var i = ratings.Length - 2; i >= 0; i--)
         {
             if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1])
